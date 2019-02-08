d74ff14b346e15297c57c3fa73c603c3f0fd5434
Amy Fouzia <afouzia00@stuy.edu>
2019-02-08 14:30:45 -0500

    added constructor + formatting

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,21 +1,23 @@
 public class QueenBoard{
-  
+
   //Instance Variables:
     private int[][]board;
 
   //Constructor:
     public QueenBoard(int size){}
 
-
   //Private methods (suggested):
-    private boolean addQueen(int r, int c)
-    private boolean removeQueen(int r, int c)
+    private boolean addQueen(int r, int c){
 
+    }
 
+    private boolean removeQueen(int r, int c){
+
+    }
 
   //Public Methods:
     /**
-    *@return The output string formatted as follows:
+    *return The output string formatted as follows:
     *All numbers that represent queens are replaced with 'Q'
     *all others are displayed as underscores '_'
     *There are spaces between each symbol:
@@ -28,24 +30,28 @@ public class QueenBoard{
     *(pythonic string notation for clarity,
     *excludes the character up to the *)
     */
-    public String toString(){}
 
+    public String toString(){
 
-
-    /**
-    *@return false when the board is not solveable and leaves the board filled with zeros;
-
-    *        true when the board is solveable, and leaves the board in a solved state
-
-    *@throws IllegalStateException when the board starts with any non-zero value
-
-    */
-    public boolean solve(){}
+    }
 
     /**
-    *@return the number of solutions found, and leaves the board filled with only 0's
-    *@throws IllegalStateException when the board starts with any non-zero value
+    *return false when the board is not solveable and leaves the board filled with zeros;
+    true when the board is solveable, and leaves the board in a solved state
+    *throws IllegalStateException when the board starts with any non-zero value
     */
-    public int countSolutions(){}
+
+    public boolean solve(){
+
+    }
+
+    /**
+    *return the number of solutions found, and leaves the board filled with only 0's
+    *throws IllegalStateException when the board starts with any non-zero value
+    */
+
+    public int countSolutions(){
+
+    }
 
 }
