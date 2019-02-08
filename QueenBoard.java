669f056acf00367587e376cf138fa6d52bb11aa9
Amy Fouzia <afouzia00@stuy.edu>
2019-02-08 14:39:15 -0500

    added private method code(for both)

--- QueenBoard.java
+++ QueenBoard.java
@@ -4,15 +4,27 @@ public class QueenBoard{
     private int[][]board;
 
   //Constructor:
-    public QueenBoard(int size){}
+    public QueenBoard(int size){
+      board = new int[size][size];
+    }
 
   //Private methods (suggested):
     private boolean addQueen(int r, int c){
+      if(!isThreatened){
+        board[r][c] = -1;
+        return true
+      }
 
+      return false;
     }
 
     private boolean removeQueen(int r, int c){
+      if(board[r][c] == -1){
+        board[r][c] = 0;
+        return true;
+      }
 
+      return false;
     }
 
   //Public Methods:
